0fbd6f3d6e8ae194f101c6d2d71c8fe5e23ea562
Abraao Lincol <abraao.lincol@gmail.com>
2021-04-27 15:56:42 -0300

    fix anotações em File

--- management/File.java
+++ management/File.java
@@ -4,49 +4,48 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class File {
-    private /*@ public_spec*/ long size;
-    private /*@ public_spec*/ String text;
+    private /*@ spec_public @*/ long size;
+    private /*@ spec_public @*/ String text;
     
     //@ public invariant size >= 0;
-    //@ public invariant text != null;
     
     /*@ requires size >= 0;
-     *@ assignable size;
-     *@ ensures this.size == size;
-     * */
+     @  assignable this.size;
+     @  ensures this.size == size;
+     @*/
     public File(long size) {
         this.size = size;
     }
     
     /*@ requires size >= 0;
-     *@ requires text != null;
-     *@ assignable size, text;
-     *@ ensures this.size == size;
-     *@ ensures this.text == text;
-     * */
+     @  requires text != null;
+     @  assignable this.size, this.text;
+     @  ensures this.size == size;
+     @  ensures this.text == text;
+     @*/
     public File(long size, String text) {
         this.size = size;
         this.text = text;
     }
     
     /*@ requires this.size >= 0;
-     *@ ensures \result == this.size;
-     * */
+    @  ensures \result == this.size;
+    @*/
     public /*@ pure @*/ long getSize() {
         return size;
     }
     
     /*@ requires this.text != null;
-     *@ ensures \result == this.text;
-     * */
+     @  ensures \result.equals(this.text);
+     @*/
     public /*@ pure @*/ String getText() {
         return text;
     }
     
     /*@ requires pathname != null;
-     *@ ensures \result.getSize() >= 0;
-     *@ ensures \result.getText() != null;  
-     * */
+     @  ensures_redundantly \result.getSize() >= 0;
+     @  ensures_redundantly \result.getText() != null;  
+     @*/
     public static File read(String pathname) {
         java.io.File file = new java.io.File(pathname);
         BufferedReader fis = null;
@@ -76,17 +75,20 @@ public class File {
     }
     
     /*@ requires pathname != null;
-     *@ ensures \result.getSize() >= 0;
-     *@ ensures \result.getText() != null;  
-     *@ ensures (\forall int i; \result.getSize() > 0 && (0 <= i < \result.getText().length()); 	  \result.getText().charAt(i) == 0 || \result.getText().charAt(i) == 1);  
-     * */
+     @  ensures_redundantly \result.getSize() >= 0;
+     @  ensures_redundantly \result.getText() != null;
+     @  ensures (\forall int i; \result.getSize() > 0 && 0 <= i && i < \result.getText().length(); \result.getText().charAt(i) == '0' || \result.getText().charAt(i) == '1');  
+     @*/
     public static File readByte(String pathname) {
         java.io.File file = new java.io.File(pathname);
         FileInputStream in = null;
         String text = "";
         try {
-            in = new FileInputStream(pathname);
-            byte[] bytes = in.readAllBytes();
+            //in = new FileInputStream(pathname);
+            //byte[] bytes = in.readAllBytes();
+        	byte[] bytes = new byte[(int) file.length()];
+        	DataInputStream dataInputStream = new DataInputStream(new FileInputStream(pathname));
+        	dataInputStream .readFully(bytes);
 
             StringBuilder sb = new StringBuilder();
             int i = 0;
@@ -105,14 +107,15 @@ public class File {
                 ex.printStackTrace();
             }
         }
-
+        
+        //String t = "10001100";
         return new File(file.length(), text);
     }
     
     /*@ requires pathname != null;
-     *@ ensures \result.getSize() >= 0;
-     *@ ensures \result.getText() != null;  
-     * */
+     @  ensures_redundantly \result.getSize() >= 0;
+     @  ensures_redundantly \result.getText() != null;  
+     @*/
     public static File readExtract(String pathname) {
         java.io.File file = new java.io.File(pathname);
         BufferedReader fis = null;
@@ -140,9 +143,9 @@ public class File {
     }
     
     /*@ requires pathname != null;
-     *@ requires content != null;
-     *@ ensures \result.getSize() >= 0;  
-     * */
+     @  requires content != null;
+     @  ensures_redundantly \result.getSize() >= 0;  
+     @*/
     public static File write(String pathname, byte[] content) {
         java.io.File file = new java.io.File(pathname);
         FileOutputStream fos = null;
@@ -163,5 +166,35 @@ public class File {
 
         return new File(file.length());
     }
+    
+    public static void main(String[] args) {
+//		File f = read("/home/abraao/Documentos/java/logica/log");
+//    	File f = readByte("/home/abraao/Documentos/java/logica/log");
+//    	File f = readExtract("/home/abraao/Documentos/java/logica/log");
+//		System.out.println(f.getSize());
+//		System.out.println(f.getText().equals(null));
+//		System.out.println(f.getText());
+//		System.out.println(f.getText().length());
+    	//Teste
+    	
+    	//File f = new File(-1);
+    	//File f = new File(10, "/asdas/dasda");
+    	
+    	//File f = new File(10);
+    	//System.out.println(f.getSize());
+    	
+    	//File f = new File(6, "teste");
+    	//System.out.println(f.getText());
+    	
+    	//File f = read("/home/abraao/Documentos/java/logica/log");
+		//System.out.println(f.getSize());
+		//System.out.println(f.getText().equals(null));
+    	
+    	//File f = readByte("/home/abraao/Documentos/java/logica/log");
+		//System.out.println(f.getSize());
+		//System.out.println(f.getText().length());
+		//System.out.println(f.getText().equals(null));
+		//System.out.println(f.getText());
+		
+	}
 }
-
